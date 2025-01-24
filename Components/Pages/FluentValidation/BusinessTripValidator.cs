8645e46a9d12ce52da3ed4f13bbe671a83d4ba40
Marcin <marcin.jarco@porscheinterauto.pl>
2025-01-24 11:50:34 +0100

    Fixed Validation in Child Component triggered from Parent.

--- Components/Pages/FluentValidation/BusinessTripValidator.cs
+++ Components/Pages/FluentValidation/BusinessTripValidator.cs
@@ -7,7 +7,9 @@ public class BusinessTripValidator : AbstractValidator<BusinessTrip>
 
     public BusinessTripValidator()
     {
-        RuleFor(x => x.EmployeeName).NotEmpty().WithMessage("Ooo here you got a problem man!");
+        RuleFor(x => x.EmployeeName)
+            .NotEmpty().WithMessage("Ooo here you got a problem man!")
+            .MinimumLength(3).WithMessage("Employee name must be at least 3 characters long.");
         RuleFor(x => x.MileageRegister).SetValidator(new MileageRegisterValidator());
 
         //Entries).ChildRules(mil =>
